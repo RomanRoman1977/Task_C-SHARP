720bd5af81041c432aac3dc497842837dd5246e7
Roman <kormakoff.roma@gmail.com>
2022-08-11 22:05:26 +0300

    sqrt проверка квадрата числа

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу которая 
+// на вход принимает два числа и проверяет
+// является ли первое число квадратом второго
+// a = 25 b = 5 -> да
+// a = 2 b = 10 ->  нет
+// a = 9 b = -3 -> да
+// a = -3 b = 9 ->  нет
+// if(number == 5) - если равно 5
+// if(number != 5) - если не равно 5
+
+
+Console.WriteLine("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int sqrt = number2 * number2;
+if(number1 == sqrt)
+{
+    Console.WriteLine($"Число {number1} является квадратом числа {number2}");
+}
+else
+{
+    Console.WriteLine($"Число {number1} не является квадратом числа {number2}");
+}
+
+
+
