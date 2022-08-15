5a2060e92942e8b641c5605ad5583066e074d2b6
Roman <kormakoff.roma@gmail.com>
2022-08-15 17:46:21 +0300

    Дни недели2

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -44,19 +44,19 @@ Console.WriteLine("Введите число от 1 до 7: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
 if(num == 1) {
-    Console.WriteLine($"{num} - Вторник");
+    Console.WriteLine($"{num} - Понедельник");
 } else if(num == 2) {
     Console.WriteLine($"{num} - Вторник");
 } else if(num == 3) {
-    Console.WriteLine($"{num} - Вторник");
+    Console.WriteLine($"{num} - Среда");
 } else if(num == 4) {
-    Console.WriteLine($"{num} - Вторник");
+    Console.WriteLine($"{num} - Четверг");
 } else if(num == 5) {
-    Console.WriteLine($"{num} - Вторник");
+    Console.WriteLine($"{num} - Пятница");
 } else if(num == 6) {
-    Console.WriteLine($"{num} - Вторник");
+    Console.WriteLine($"{num} - Суббота");
 } else if(num == 7) {
-    Console.WriteLine($"{num} - Вторник");
+    Console.WriteLine($"{num} - Воскресенье");
 } else {
     Console.WriteLine("Введено не корректное число");
 };
