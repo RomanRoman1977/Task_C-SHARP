9ed1c87c8cd6f93ca8f2d007b34ed5dbd20a27a4
Roman <kormakoff.roma@gmail.com>
2022-08-15 23:09:09 +0300

    Multiplicity

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 12 Напишите программу которая будет принимать
+на вход два числа и выводить является ли второе число
+кратным первому. Если число 2 не кратно числу 1 то программа
+выводит остаток от деления
+34, 5 - не кратно, остаток 4
+16, 4 - кратно*/
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+void Multiplicity(int num1, int num2)
+{
+    if(num1 % num2 == 0)
+    {
+        Console.WriteLine($"{num1} кратно {num2}");
+    }
+    else
+    {
+        Console.WriteLine($"{num1} не кратно {num2}, остаток от деления = " + num1 % num2);
+    }
+}
+Multiplicity(number1, number2);
+
+
+
