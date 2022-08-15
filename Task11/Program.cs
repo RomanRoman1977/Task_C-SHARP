a1d619b71a6d769ca53782677b094fd035137fb4
Roman <kormakoff.roma@gmail.com>
2022-08-15 22:17:45 +0300

    вывод первой и последней цифры трехзначного сгенерированного числа

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -4,4 +4,15 @@
 782 - 72
 918 - 98*/
 
-int number = new Random().Next(10, 100);
+int DeleteMiddleDigit(int num)
+{
+    int firstDigit = num / 100;
+    int secondDigit = num % 10;
+    int result = firstDigit * 10 + secondDigit;
+    return result;
+}
+
+int number = new Random().Next(100, 1000);
+
+int res = DeleteMiddleDigit(number);
+Console.WriteLine($"Сгенерированное число {number}, обработанное число {res}");
