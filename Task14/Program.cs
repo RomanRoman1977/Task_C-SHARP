3e3f4ea4569a1e503ff25b173bb5564ff1a629d4
Roman <kormakoff.roma@gmail.com>
2022-08-16 00:14:44 +0300

    кратно двум числам

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 14 Напишите програииу которая принимает
+на вход число и проверяет кратно ли оно 7 и 23
+14 - нет
+46 - нет
+161 - до*/
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool Multiplicity(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
+bool result = Multiplicity(number);
+Console.WriteLine(result ? "Да" :  "Нет");
+
+
+
