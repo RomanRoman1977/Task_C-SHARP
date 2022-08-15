4890afeb76af13aa9794e9bb534111b62de85e48
Roman <kormakoff.roma@gmail.com>
2022-08-16 00:40:42 +0300

    проверка квадрата числа

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу которая принимает на вход
+два числа и проверяет является ли одно число 
+квадратом другого
+5, 25 - да
+-4, 16 - да
+25, 5 - да
+8, 9 - нет*/
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool Square(int num1, int num2)
+{
+    return (num1 * num1 == num2 || num2 * num2 == num1);
+}
+bool result = Square(number1, number2);
+Console.WriteLine(result ? "Да" : "Нет");
+
+
