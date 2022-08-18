f5810cbf353ef8970b5f175e754354a1bc50b7ac
Roman <kormakoff.roma@gmail.com>
2022-08-18 18:24:30 +0300

    Вторая цифра трехзначного числа

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*10: Напишите программу,
+ которая принимает на вход 
+ трёхзначное число и на выходе
+  показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <= 999)
+{
+    int firstDigit = number / 100 * 10;
+    int secondDigit = number / 10;
+    int result = secondDigit - firstDigit;
+    Console.WriteLine($"Вторая цифра введенного числа {result}");
+}
+else
+{
+    Console.WriteLine("Введено некорректное число");
+}
