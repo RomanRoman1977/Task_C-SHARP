9f21be98f2b76d6ded0d60198ee20279dc1ef098
Roman <kormakoff.roma@gmail.com>
2022-08-23 23:08:11 +0300

    третья цифра введеного числа

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*13: Напишите программу, которая выводит третью цифру 
+заданного числа или сообщает, что третьей цифры нет. 
+Решить без использования строк.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = 0;
+
+if(num > 99)
+{
+    while(num >= 1000)
+    {
+        num = (num / 10);
+    }
+    res = num % 10;
+    Console.WriteLine($"Третья цифра введенного числа: {res}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры в введенном числе нет");
+}
+
+
+   
