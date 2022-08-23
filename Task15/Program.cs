1ec265f40df7044041dc9883315468c0f9d327bf
Roman <kormakoff.roma@gmail.com>
2022-08-24 00:05:42 +0300

    Проверка выходного дгя недели

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 15: Напишите программу, 
+которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, 
+является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет*/
+
+Console.WriteLine("Введите день недели от 1 до 7");
+int num = Convert.ToInt32(Console.ReadLine());
+string day = "";
+
+if (num >= 1 && num <= 7)
+{
+    switch (num)
+    {
+        case 1:
+            day = "нет, это не выходной день недели.";
+            break;
+        case 2:
+            day = "нет, это не выходной день недели.";
+            break;
+        case 3:
+            day = "нет, это не выходной день недели.";
+            break;
+        case 4:
+            day = "нет, это не выходной день недели.";
+            break;
+        case 5:
+            day = "нет, это не выходной день недели.";
+            break;
+        case 6:
+            day = "да, это выходной день недели!";
+            break;
+        case 7:
+            day = "да, это выходной день недели!";
+            break;
+    }
+    Console.WriteLine(day);
+}
+else
+{
+    Console.WriteLine("Введено некорректное число");
+}
