d885945e645b2b54a6d4d4d5cdf7d2921663031a
Roman <kormakoff.roma@gmail.com>
2022-08-24 02:54:14 +0300

    Палиндром

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу,
+ которая принимает на вход пятизначное число
+  и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num > 100000 || num < 10000)
+{
+  Console.WriteLine("Введено некорректное число");
+}
+else if(num / 1000 == ((num % 10 * 10) + ((num % 100 - num % 10) / 10)))
+{
+  Console.WriteLine("Это число палиндром");
+}
+else if (num / 1000 != ((num % 10 * 10) + ((num % 100 - num % 10) / 10)))
+{
+  Console.WriteLine("Это число не палиндром");
+}
