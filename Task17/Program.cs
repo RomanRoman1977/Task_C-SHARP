5048fa3379e57ffeb404aaa205ac9c2bbbaf967b
Roman <kormakoff.roma@gmail.com>
2022-08-24 19:50:39 +0300

    Определение четверти по координатам2

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -7,29 +7,25 @@ ____|______
   3 | 4
     |
 */
- Console.WriteLine("Введите координаты точки");
- Console.Write("X: ");
- int x = Convert.ToInt32(Console.ReadLine());
- Console.Write("Y: ");
- int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки");
+Console.Write("X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
 
- if(x > 0 && y > 0)
- {
-    Console.WriteLine("Первая четверть");
- }
- else if(x > 0 && y > 0)
- {
-    Console.WriteLine("Вторая четверть");
- }
-else if(x < 0 && y < 0)
- {
-    Console.WriteLine("Третья четверть");
- }
- else if(x > 0 && y < 0)
- {
-    Console.WriteLine("Четвертая четверть");
- }
- else
- {
-    Console.WriteLine("Введены некорректные координаты");
- }
+/*if (x > 0 && y > 0) Console.WriteLine("Первая четверть");
+else if (x > 0 && y > 0) Console.WriteLine("Вторая четверть");
+else if (x < 0 && y < 0) Console.WriteLine("Третья четверть");
+else if (x > 0 && y < 0) Console.WriteLine("Четвертая четверть");
+else Console.WriteLine("Введены некорректные координаты");*/
+
+string Quarter(int xc, int yc)
+{
+    if (xc > 0 && yc > 0) return "Первая четверть";
+    if (xc > 0 && yc > 0) return "Вторая четверть";
+    if (xc < 0 && yc < 0) return "Третья четверть";
+    if (xc > 0 && yc < 0) return "Четвертая четверть";
+    return "Введены некорректные координаты";
+}
+string result = Quarter(x, y);
+Console.WriteLine(result);
