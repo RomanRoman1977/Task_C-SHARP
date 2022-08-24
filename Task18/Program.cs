adc659e2c7b9e2007b3dc5260cf97b7d3be5342b
Roman <kormakoff.roma@gmail.com>
2022-08-24 20:14:34 +0300

    По номеру четверти диапазон координат

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 18 Напишите программу которая
+по заданному номеру четверти
+покахывает диапазон возможных
+координат точек в этой четверти (x и y)*/
+
+Console.WriteLine("Введите номер четверти: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string Del(int number)
+{
+    if (number == 1) return "x = от 1 до N, y = от 1 до N";
+    if (number == 2) return "x = от 1 до N, y = от -1 до -N";
+    if (number == 3) return "x = от -1 до -N, y = от -1 до -N";
+    if (number == 4) return "x = от 1 до N, y = от -1 до -N";
+    return "Введена некорректная четверть";
+}
+string result = Del(num);
+Console.WriteLine(result);
+
