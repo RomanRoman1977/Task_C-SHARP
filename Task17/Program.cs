06c5856c3ff055ccc0de204ee5f7bc93f6c05f45
Roman <kormakoff.roma@gmail.com>
2022-08-24 19:14:25 +0300

    Определение четверти по координатам

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 17 Напишите программу которая принимает на вход
+координаты точки (X Y),причем X !=0 Y!=0 и выдает номер четвыерти плоскости
+в которой находится эта точка.
+
+  2 | 1
+____|______
+  3 | 4
+    |
+*/
+ Console.WriteLine("Введите координаты точки");
+ Console.Write("X: ");
+ int x = Convert.ToInt32(Console.ReadLine());
+ Console.Write("Y: ");
+ int y = Convert.ToInt32(Console.ReadLine());
+
+ if(x > 0 && y > 0)
+ {
+    Console.WriteLine("Первая четверть");
+ }
+ else if(x > 0 && y > 0)
+ {
+    Console.WriteLine("Вторая четверть");
+ }
+else if(x < 0 && y < 0)
+ {
+    Console.WriteLine("Третья четверть");
+ }
+ else if(x > 0 && y < 0)
+ {
+    Console.WriteLine("Четвертая четверть");
+ }
+ else
+ {
+    Console.WriteLine("Введены некорректные координаты");
+ }
