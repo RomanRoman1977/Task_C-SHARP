8edc4acd6fb3ce7e60a212da6452204350111bf1
Roman <kormakoff.roma@gmail.com>
2022-08-27 12:11:56 +0300

    Выдает таблицу кубов от 1 до N

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 23 Напишате программу которая принимает
+на вход число (N) и выдает таблицу кубов числе
+от 1 до N
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while(count <= n)
+{
+    Console.WriteLine($"{count} | {Math.Pow(count, 3)}");// 2 - возводит в квадрат, 3 - в куб и т.д.
+    count++;
+}
