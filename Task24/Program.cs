3aefc8813bc06e37010e84cedf2a59900d4634f8
Roman <kormakoff.roma@gmail.com>
2022-08-27 15:04:02 +0300

    Сумма цифр введенного числа

--- Task24/Program.cs
+++ Task24/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу которая принимает на вход число (А)
+и выдает сумму чисел от 1 до А
+7-28
+4-10
+8-36*/
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+for(int i = 1; i <= num; i++)
+{
+    // sum = sum + i;
+    sum +=i;
+}
+Console.WriteLine($"Сумма чисел от 1 до {num} = {sum}");
