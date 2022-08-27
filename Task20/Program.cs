8a7d0ccfb8314cf901f39df8137c773d1a41cdef
Roman <kormakoff.roma@gmail.com>
2022-08-27 10:48:47 +0300

    Расстояние между точками теор Птфагора

--- Task20/Program.cs
+++ Task20/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 20 Напишите программу которая
+принимает на вход координаты двух точек
+и находит расстояние между ними в 2D пространстве
+А (3,6); В (2,1) -> 5,09
+А (7,-5); В (1,-1) -> 7,21*/
+
+Console.WriteLine("Введите координаты точки A");
+Console.Write("x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B");
+Console.Write("x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double Pifagor(int xc1, int yc1, int xc2, int yc2)
+{
+    return Math.Sqrt((xc2 - xc1)*(xc2 - xc1) + (yc2 - yc1)*(yc2 - yc1));
+}
+double result = Pifagor(x1, y1, x2, y2);
+Console.WriteLine($"Расстояние между точками A {x1},{y1} и B {x2},{y2} = {result}");
+
+
+
