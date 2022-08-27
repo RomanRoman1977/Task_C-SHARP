3b0fdf366af4a889da7664a5f55002de90d695e8
Roman <kormakoff.roma@gmail.com>
2022-08-27 11:01:04 +0300

    Расст м/д точками теор Пифагора

--- Task20/Program.cs
+++ Task20/Program.cs
@@ -20,7 +20,7 @@ double Pifagor(int xc1, int yc1, int xc2, int yc2)
     return Math.Sqrt((xc2 - xc1)*(xc2 - xc1) + (yc2 - yc1)*(yc2 - yc1));
 }
 double result = Pifagor(x1, y1, x2, y2);
-Console.WriteLine($"Расстояние между точками A {x1},{y1} и B {x2},{y2} = {result}");
-
+Console.WriteLine($"Расстояние между точками A {x1},{y1} и B {x2},{y2} = {Math.Round(result,1)}");
+// Math.Round - округляет результат до необходимого нам знака.
 
 
