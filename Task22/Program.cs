342bf69d7c40b7e2771e2e0735837b3e983ef85c
Roman <kormakoff.roma@gmail.com>
2022-08-27 12:06:34 +0300

    вывод таблицы квадратов от 1 до n

--- Task22/Program.cs
+++ Task22/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 22 Напишате программу которая принимает
+на вход число (N) и выдает таблицу квадратов
+от 1 до N
+5 - 1,4,9,16,25
+2 - 1,4
+*/
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while(count <= n)
+{
+    Console.WriteLine($"{count} | {Math.Pow(count, 2)}");// 2 - возводит в квадрат, 3 - в куб и т.д.
+    count++;
+}
+
+
+
+
+
