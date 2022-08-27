24c0aee90de988d8a3800ac0da244490ed927b5e
Roman <kormakoff.roma@gmail.com>
2022-08-27 12:30:51 +0300

    Расстояние м/д двумя точками в 3D пространстве

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -9,18 +9,23 @@ Console.Write("x1: ");
 int x1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("y1: ");
 int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите координаты точки B");
 Console.Write("x2: ");
 int x2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("y2: ");
 int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
 
-double Pifagor(int xc1, int yc1, int xc2, int yc2)
+
+double Pifagor(int xc1, int yc1, int zc1, int xc2, int yc2, int zc2)
 {
-    return Math.Sqrt((xc2 - xc1)*(xc2 - xc1) + (yc2 - yc1)*(yc2 - yc1));
+    return Math.Sqrt((xc2 - xc1)*(xc2 - xc1) + (yc2 - yc1)*(yc2 - yc1) + (zc2 - zc1)*(zc2 - zc1));
 }
-double result = Pifagor(x1, y1, x2, y2);
-Console.WriteLine($"Расстояние между точками A {x1},{y1} и B {x2},{y2} = {result}");
+double result = Pifagor(x1, y1, z1, x2, y2, z2);
+Console.WriteLine($"Расстояние между точками A {x1},{y1},{z1} B {x2},{y2},{x2} = {Math.Round(result, 2)}");
 
 
 
