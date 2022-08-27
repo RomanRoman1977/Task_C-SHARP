ad3e7cd76d440c5982747dee02790c634fb85c9d
Roman <kormakoff.roma@gmail.com>
2022-08-27 16:36:51 +0300

    Количество цифр в введенном числе

--- Task26/Program.cs
+++ Task26/Program.cs
@@ -4,4 +4,42 @@
 78 - 2
 89126 - 5*/
 
+// Решаем через while
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int count = 1;
 
+// while(num >= 10)
+// {
+//     num = num / 10;
+//     count++;
+// }
+// Console.WriteLine($"Количество цифр в числе = {count}");
+
+// Решаем через for
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int count = 1;
+
+// for (; num >= 10; num = num / 10)
+// {
+//     count++;
+// }
+// Console.WriteLine($"Количество цифр в числе = {count}");
+
+// Решаем через метод
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+int CountNumber(int num)
+{
+    while (num >= 10)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+int res = CountNumber(number);
+Console.WriteLine($"Количество цифр в числе {number} = {res}");
