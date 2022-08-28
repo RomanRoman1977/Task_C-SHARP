25cc41bc7393de76a094dbce38cc7ffd4cee4be5
Roman <kormakoff.roma@gmail.com>
2022-08-28 22:23:03 +0300

    Вывод масс из 8 эле

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -4,3 +4,27 @@
 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
 */
 
+int[] result = CreateArray(8, 0, 100);
+PrintArr(result);
+
+int[] CreateArray(int n, int min, int max)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+    return array;
+}
+
+void PrintArr(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write(array[i] + ",");
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.Write("]");
+}
+
