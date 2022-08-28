cb3c43a0b07643216f712f8326cb6cbf6f1efff0
Roman <kormakoff.roma@gmail.com>
2022-08-28 22:10:16 +0300

    Вывод масс из 8 элем [0,1..]

---
+++ Task30/Program.cs
@@ -0,0 +1,41 @@
+﻿/* 30: Напишите программу которая
+выводит массив из 8 элементов заполненный
+нулями и единицами в случайном порядке
+[1,0,1,1,0,1,0,0]  */
+
+int[] result = CreateArray(8, 0, 2);
+PrintArr(result);
+
+int[] CreateArray(int n, int min, int max)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+    return array;
+}
+
+/*void PrintArr(int[] array)
+{
+     for (int i = 0; i < array.Length; i++)
+    {
+        if(i == 0) Console.Write("[");
+        if(i < array.Length -1) Console.Write(array[i] + ",");
+        else Console.Write(array[i] + "]");
+    }
+}
+*/
+
+// ИЛИ ТАКОЙ ВАРИАНТ
+
+void PrintArr(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write(array[i] + ",");
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.Write("]");
+}
