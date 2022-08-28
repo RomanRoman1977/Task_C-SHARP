9ac2927e8c6d10b216ec1a20d9ee0bdc3ecfc47b
Roman <kormakoff.roma@gmail.com>
2022-08-28 19:40:53 +0300

    Возведение первого числа в натуральную степень второго

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,7 +1,19 @@
-﻿/* 225: Напишите цикл, 
+﻿/* 25: Напишите цикл, 
 который принимает на вход два числа (A и B) 
 и возводит число A в натуральную степень B.
 3, 5 -> 243 (3⁵)
 2, 4 -> 16*/
 
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int degree = 1;
 
+while (count <= b)
+{
+    degree = degree * a;
+    count++;
+}
+Console.WriteLine($"Число {a} возведенное в натуральную степень числа {b} = {degree}");
