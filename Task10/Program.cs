5c7f1d13c4beea3c5ad288e0fe88ba48acd3a4b2
Roman <kormakoff.roma@gmail.com>
2022-08-28 23:08:19 +0300

    Вторая цифра 3-х значного числа

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -11,12 +11,7 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 if (number >= 100 && number <= 999)
 {
-    int firstDigit = number / 100 * 10;
-    int secondDigit = number / 10;
-    int result = secondDigit - firstDigit;
+    int result = number / 10 % 10;
     Console.WriteLine($"Вторая цифра введенного числа {result}");
 }
-else
-{
-    Console.WriteLine("Введено некорректное число");
-}
+else Console.WriteLine("Введено некорректное число");
