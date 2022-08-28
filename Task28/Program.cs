b773252a4255b0dcd19ad47bcf64c11d692dde05
Roman <kormakoff.roma@gmail.com>
2022-08-28 20:44:05 +0300

    Произведение от 1 до N

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -6,3 +6,22 @@
 
 Console.WriteLine("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
+
+int MultN(int n)
+{
+    int m = 1;
+    for (int i = 1; i <= n; i++) m *= i;
+    return m;
+}
+int mult = MultN(num);
+Console.WriteLine($"Произведение числе от 1 до {num} = " + mult);
+
+// Console.WriteLine("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int mult = 1;
+
+// for (int i = 1; i <= n; i++)
+// {
+//     mult *= i;
+// }
+// Console.WriteLine($"Произведение числе от 1 до {n} = " + mult);
