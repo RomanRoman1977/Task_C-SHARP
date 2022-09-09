944b707686e52e599a24236fc6a1ca4923e71769
Roman <kormakoff.roma@gmail.com>
2022-09-09 14:46:51 +0300

    Количество четных числе в массиве

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,61 +1,47 @@
-﻿/*31. Задайте массив из 12 эоементов
- заполненный случайными числами из промежутка [-9, 9].
- Найдите сумму отрицательных и положительных
- элементов массива.
- Например в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6]
- сумма положительных чисел равна 29, сумма отрицательных
- равна -20*/
+﻿// 34: Задайте массив заполненный 
+// случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет
+//  количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
 
+int[] num = new int[10];
 
-int[] CreateArrayRndInt (int size, int min, int max)
+void CreateArrayPosElem(int[] array, int min, int max)
 {
-    int[] array = new int[size];
     Random rnd = new Random();
-
-    for (int i = 0; i < size; i++)
-    {
-        array[i] = rnd.Next(min, max + 1);
-    }
-    return array;
-}
-
-int[] GetSumPositivNegativElem(int[] array)
-{
-    int sumNeg = 0;
-    int sumPos = 0;
 
     for (int i = 0; i < array.Length; i++)
     {
-        if(array[i] < 0) 
-            sumNeg += array[i];
-
-        else 
-            sumPos += array[i];
+        array[i] = rnd.Next(min, max + 1);
     }
-    // int arr = new int[2];
-    // arr[0] = sumNeg;
-    // arr[1] = sumPos;
-    // return arr;
-    //Или можно записать одной строкой
-    return new int[] {sumNeg, sumPos};
-
 }
 
-void PrintArray (int[] array)
+void PrintArray(int[] array)
 {
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
-        if(i < array.Length - 1) Console.Write($"{array[i]},");
+        if(i < array.Length - 1) Console.Write($"{array[i]}, ");
     }
-    Console.Write(array[array.Length - 1]);
+    Console.Write(array[array.Length -1]);
     Console.Write("]");
 }
 
-int[] arr = CreateArrayRndInt(12, -9, 9);
-PrintArray(arr);
-int[] sumPositivNegativElem = GetSumPositivNegativElem(arr);
+int PositiveElements(int[] array)
+{
+    int posElem = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            posElem++;
+        }
+    }
+    return posElem;
+}
+CreateArrayPosElem(num, 100, 1000);
+PrintArray(num);
 Console.WriteLine();
-Console.WriteLine($"Сумма отрицательных числе = {sumPositivNegativElem[0]}");
-Console.WriteLine($"Сумма положительных числе = {sumPositivNegativElem[1]}");
+int posElem = PositiveElements(num);
+Console.WriteLine($"Количество чётных чисел в массиве: {posElem}");
 
